f56eb0932dda527d5e59a144a961c9cf4601c5d9
Sruthi Reddy <sruthir584@gmail.com>
2024-01-07 18:56:14 +0530

    testing

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -5,12 +5,19 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
 filePath = 'house.csv'
+# filePath = 'sampleData2SaketKapra.csv'
 df = pd.read_csv(filePath)
-
 X = df[['TIME']].values.astype(float)
 y = df['Value'].values.astype(float)
 print(df[['TIME']].values.astype(float))
 print(df['Value'].values.astype(float))
+
+print(filePath)
+area_name = input("Enter the name of the area: ")
+print("You entered:", area_name)
+units = input("Enter the units (per sq ft, per sq meter, per 100 sq meter): ")
+print(f"You entered units: {units}")
+
 scalerX = MinMaxScaler()
 XScaled = scalerX.fit_transform(X)
 scalerY = MinMaxScaler()
@@ -24,15 +31,15 @@ model.compile(optimizer='adam', loss='mean_squared_logarithmic_error')
 model.fit(XScaled, YScaled, epochs=1000, verbose=0)
 trainingPredictionsScaled = model.predict(XScaled)
 trainingPredictions = scalerY.inverse_transform(trainingPredictionsScaled).flatten()
-plt.scatter(df['TIME'], df['Value'], label='Actual')
+plt.scatter(df['TIME'], df['Value'].astype(float), label='Actual')
 plt.scatter(df['TIME'], trainingPredictions, label='Predicted (Training)', color='red')
 plt.xlabel('Year')
-plt.ylabel('₹ lakhs')
-plt.title('Actual vs Predicted Values (Training)')
+plt.ylabel('₹')
+plt.title(area_name)
 plt.legend()
 plt.show()
 
-futureYears = np.arange(2023, 2033).reshape(-1, 1)
+futureYears = np.arange(2023, 2030).reshape(-1, 1)
 futureYearsScaled = scalerX.transform(futureYears)
 futureValuesScaled = model.predict(futureYearsScaled)
 futureValues = scalerY.inverse_transform(futureValuesScaled).flatten()
@@ -40,11 +47,12 @@ plt.scatter(df['TIME'], df['Value'], label='Actual')
 plt.scatter(df['TIME'], trainingPredictions, label='Predicted (Training)', color='red')
 plt.scatter(futureYears.flatten(), futureValues, label='Predicted (Future)', color='green')
 plt.xlabel('Year')
-plt.ylabel('₹ lakhs')
-plt.title('Actual, Predicted, and Future Values (Exponential Growth)')
+plt.ylabel('₹')
+plt.title(area_name)
 plt.legend()
 plt.show()
 
-print('Predicted values for the next 10 years:')
+print('Predicted values:')
 for year, value in zip(futureYears.flatten(), futureValues):
-    print(f'Year {year}: {value}')
+    formatted_value = "{:.2f}".format(value)
+    print(f'Year {year}, Predicted Price: ₹{formatted_value}{units}')
